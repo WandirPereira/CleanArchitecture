ecc57de34f61cc913d96ef288e23e8a4d32f2f14
Wandir Pereira Filho <wandir@gmail.com>
2022-04-17 01:47:52 -0300

    Configuração inicial do projeto

---
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+
+StreamerDbContext dbContext = new();
+
+Streamer streamer = new() { 
+    Nombre = "Amazon Prime",
+    Url = "https://www.amazonprime.com"
+};
+
+dbContext!.Streamers!.Add(streamer);
+
+await dbContext.SaveChangesAsync();
+
+var movies = new List<Video> {
+    new Video
+    {
+        Nombre = "Mad Max",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Batman",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Crepusculo",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Citzen Kanex",
+        StreamerId = streamer.Id
+    },
+};
+
+await dbContext.AddRangeAsync(movies);
+await dbContext.SaveChangesAsync();
+
---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Data Source=localhost; Initial Catalog=Streamer; Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Data Source=localhost; Initial Catalog=Streamer;user id=sa;password=senha@1234")
+                .LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name}, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
